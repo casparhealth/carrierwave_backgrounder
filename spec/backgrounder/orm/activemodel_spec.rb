864372b895278dc61c06924701776086590262de
Ahmed Helil <aaeabdo@gmail.com>
2018-12-11 10:17:07 +0100

    BUG-65 fix deprecation warning  (#1)
    * BUG-65 replace deprecated method
    
    * BUG-65 pump gem version

--- spec/backgrounder/orm/activemodel_spec.rb
+++ spec/backgrounder/orm/activemodel_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe CarrierWave::Backgrounder::ORM::ActiveModel do
     @mock_class = Class.new do
       def self.before_save(method, opts); nil; end
       def self.after_commit(method, opts); nil; end
-      def avatar_changed?; nil;  end
+      def saved_change_to_avatar?; nil;  end
       def remote_avatar_url; OpenStruct.new(:present? => true); end
       def remove_avatar?; false; end
       def previous_changes; {}; end
@@ -54,7 +54,7 @@ RSpec.describe CarrierWave::Backgrounder::ORM::ActiveModel do
       end
 
       it "returns true if alternate column is changed" do
-        expect(instance).to receive(:some_other_column_changed?).and_return(true)
+        expect(instance).to receive(:saved_change_to_some_other_column?).and_return(true)
         expect(instance.avatar_updated?).to be_truthy
       end
     end
@@ -66,20 +66,20 @@ RSpec.describe CarrierWave::Backgrounder::ORM::ActiveModel do
 
     it "calls column_changed?" do
       expect(instance).to receive(:process_avatar_upload).and_return(false)
-      expect(instance).to receive(:avatar_changed?)
+      expect(instance).to receive(:saved_change_to_avatar?)
       expect(instance.enqueue_avatar_background_job?).to be_truthy
     end
 
     it "calls previous_changes" do
       expect(instance).to receive(:process_avatar_upload).and_return(false)
-      expect(instance).to receive(:avatar_changed?).and_return(false)
+      expect(instance).to receive(:saved_change_to_avatar?).and_return(false)
       expect(instance).to receive(:previous_changes).and_return({:avatar => true})
       expect(instance.enqueue_avatar_background_job?).to be_truthy
     end
 
     it "calls avatar_remote_url" do
       expect(instance).to receive(:process_avatar_upload).and_return(false)
-      expect(instance).to receive(:avatar_changed?).and_return(false)
+      expect(instance).to receive(:saved_change_to_avatar?).and_return(false)
       expect(instance).to receive(:previous_changes).and_return({})
       expect(instance).to receive(:remote_avatar_url).and_return('yup')
       expect(instance.enqueue_avatar_background_job?).to be_truthy
@@ -87,7 +87,7 @@ RSpec.describe CarrierWave::Backgrounder::ORM::ActiveModel do
 
     it "calls avatar_cache" do
       expect(instance).to receive(:process_avatar_upload).and_return(false)
-      expect(instance).to receive(:avatar_changed?).and_return(false)
+      expect(instance).to receive(:saved_change_to_avatar?).and_return(false)
       expect(instance).to receive(:previous_changes).and_return({})
       expect(instance).to receive(:remote_avatar_url).and_return(nil)
       expect(instance).to receive(:avatar_cache).and_return('yup')
